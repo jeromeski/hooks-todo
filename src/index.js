b270bee6bb5379760081bffbd6ee111b969d9249
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2021-04-03 16:11:52 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newInput = { id: todos.length + 1, name: input };
+    setTodos((prev) => [...prev, newInput]);
+    setInput("");
+  };
+
+  const handleDelete = (todo) => {
+    const idx = todos.findIndex((t) => t.id === todo.id);
+    setTodos((prev) => prev.splice(0, idx));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={input}
+          type="text"
+          onChange={handleChange}
+          placeholder="Enter Todo"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.name}
+          <button onClick={() => handleDelete(todo)}>Delete</button>
+        </li>
+      ))}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Todo />, rootElement);
